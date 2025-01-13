01c87d0a072ed18c180ad7e62f0f85c4c1ce0232
JP Ricarse <johnpauliloilo@gmail.com>
2025-01-14 09:08:56 +1300

    add an input validator

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -2,3 +2,13 @@
 var dice = new DiceRoll.Dice();
 dice.Roll();
 Console.WriteLine("A dice has been rolled, you have 3 tries to guess the number that was rolled.");
+
+bool successGuess = false;
+
+for(int i = 0; i < 3; i++)
+{
+  if(successGuess)
+  {
+    break;
+  }
+}
---
+++ DiceRoll/Validator/Input.cs
@@ -0,0 +1,36 @@
+﻿namespace DiceRoll.Validator;
+
+public class Input
+{
+  public string ErrorMessage { get; private set; }
+  public bool Validate(string input)
+  {
+    bool parsable = int.TryParse(input, out int guessNumber);
+    
+    if(!parsable)
+    {
+      ErrorMessage = "Input is non parsable";
+      return false;
+    }
+    
+    if(!isInRange(guessNumber))
+    {
+      ErrorMessage = "Input is not in acceptable range";
+      return false;
+    }
+
+    return true;
+  }
+
+  public string ValidationError()
+  {
+    
+    return "";
+  }
+
+  private bool isInRange(int guessNumber)
+  {
+    return (guessNumber is > 0 and <= 6);
+  }
+}
+
